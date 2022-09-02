6917005eea20635e6af29cc3e4c58587085ea143
MosNes <7hekarl@gmail.com>
2022-09-02 13:20:31 -0400

    updated for heroku staging

--- server/routes/htmlRoutes.js
+++ server/routes/htmlRoutes.js
@@ -2,6 +2,6 @@ const path = require('path');
 
 module.exports = function(app) {
     app.get('/', function(req, res) {
-        res.sendfile(path.join(__dirname, '../../client/index.html'));
+        res.sendfile(path.join(__dirname, '../../client/dist/index.html'));
     });
 };
