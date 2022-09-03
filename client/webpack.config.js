f20d2763c2e086cced38257db8792fa6e23cc25f
MosNes <7hekarl@gmail.com>
2022-09-03 19:21:08 -0400

    added and configured workbox caching strategy

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: "./src/js/index.js",
@@ -11,6 +12,27 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './index.html',
             title: 'Webpack Plugin'
+        }),
+        new WorkboxPlugin.GenerateSW({
+            //do not precache images
+            exclude: [/\.(?:png|jpg|jpeg|svg)$/],
+            //define runtime caching rules
+            runtimeCaching: [{
+                //match any request that ends with .svg, .jpg, .jpeg, or .png
+                urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+                
+                //use the cache-first strategy (try to send response from cache, then if it's not available, fetch from network)
+                handler: 'CacheFirst',
+
+                options: {
+                    //use custom cache name
+                    cacheName: 'images',
+                    //only cache 1 image
+                    expiration: {
+                        maxEntries: 1,
+                    },
+                },
+            }],
         })
     ],
     module: {
@@ -30,11 +52,11 @@ module.exports = {
                     loader: 'babel-loader',
                     options: {
                         presets: [
-                            ['@babel/preset-env', {targets: 'defaults'}]
+                            ['@babel/preset-env', { targets: 'defaults' }]
                         ]
                     }
                 }
-            } 
+            }
         ]
     }
 };
